3b984b2a0f4ce5694474a2eb74c4024542e8beef
crisp <crispsimp@outlook.com>
2021-01-24 19:20:58 +0000

    Chapter 2 - additional behaviour

--- Chapter02-Variables/Program.cs
+++ Chapter02-Variables/Program.cs
@@ -6,16 +6,20 @@ namespace Chapter02_Variables
     {
         static void Main(string[] args)
         {
-            //Set variables types
-            string myName;
-            char myInitial;
-            
-            //initialise variables
-            myName = "Structure Vault";
-            myInitial = 'S';
+            double price = 14.56;
+            Console.WriteLine("price: {0}", price);
+            Console.WriteLine("sizeof: {0}", sizeof(double));
+            Console.WriteLine("default: {0}", default(double));
+            Console.WriteLine("min value: {0}", double.MinValue);
+            Console.WriteLine("max value: {0}", double.MaxValue);
+            Console.WriteLine("type: {0}", price.GetType());
+            Console.WriteLine("code: {0}", price.GetTypeCode());
+            Console.WriteLine("price: £{0}", price.ToString());
 
-            //write to the console window
-            Console.WriteLine("my name is: {0} and my initial is: {1}", myName, myInitial);
+            string myName = "Structure Vault";
+            Console.WriteLine("my name: {0}", myName);
+            Console.WriteLine("type: {0}", myName.GetType());
+            Console.WriteLine("code: {0}", myName.GetTypeCode());
         }
     }
 }
