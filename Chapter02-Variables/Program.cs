1738aea7cc6f4e66dfd4e1d5121adc2e4ff0fa4b
crisp <crispsimp@outlook.com>
2021-01-24 19:05:52 +0000

    Chapter 2 - strings

--- Chapter02-Variables/Program.cs
+++ Chapter02-Variables/Program.cs
@@ -6,41 +6,16 @@ namespace Chapter02_Variables
     {
         static void Main(string[] args)
         {
-            //This is a single line comment
-
-            /* This is a
-             multi-line
-             comment */
-
-            Console.WriteLine("Hello World!");
-
-            sbyte a = 10;
-            byte b = 12;
-            short c = -45;
-            ushort d = 25406;
-            int e = 5689445;
-            uint f = 899888998;
-            long g = -4554285618754;
-            ulong h = 18446744073709551615;
-            float j = 1.5f;
-            double k = 16.586;
-            decimal m = 156.68m;
-
-            Console.WriteLine("sbyte: " + a);
-            Console.WriteLine("byte: " + b);
-            Console.WriteLine("short: " + c);
-            Console.WriteLine("ushort: " + d);
-            Console.WriteLine("int: " + e);
-            Console.WriteLine("uint: " + f);
-            Console.WriteLine("long: " + g);
-            Console.WriteLine("ulong: " + h);
-            Console.WriteLine("float: " + j);
-            Console.WriteLine("double: " + k);
-            Console.WriteLine("decimal: " + m);
+            //Set variables types
+            string myName;
+            char myInitial;
             
-            Console.WriteLine("sbyte: {0}", a);
-            
-            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            //initialise variables
+            myName = "Structure Vault";
+            myInitial = 'S';
+
+            //write to the console window
+            Console.WriteLine("my name is: {0} and my initial is: {1}", myName, myInitial);
         }
     }
 }
