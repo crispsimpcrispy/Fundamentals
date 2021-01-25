004e4ac760fe3385b3079712b94dde6e47a0fe15
crisp <crispsimp@outlook.com>
2021-01-25 22:01:46 +0000

    chapter04-LISTS

--- Chapter04-Collections/Program.cs
+++ Chapter04-Collections/Program.cs
@@ -2,50 +2,28 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Chapter04_Collections
 {
-    enum Days
-    {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday, 
-        Sunday
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Days selectedDay = Days.Wednesday;
+            //Define a list of strings
+            List<string> groceries = new List<string>();
+            
+            //Add some items to the list
+            groceries.Add("Milk");
+            groceries.Add("One Egg");
+            groceries.Add("Flour");
+            groceries.Add("Butter");
 
-            switch (selectedDay)
+            foreach (var item in groceries)
             {
-                case Days.Monday:
-                    Console.WriteLine("Monday selected");
-                    break;
-                case Days.Tuesday:
-                    Console.WriteLine("Tuesday selected");
-                    break;
-                case Days.Wednesday:
-                    Console.WriteLine("Wednesday selected");
-                    break;
-                case Days.Thursday:
-                    Console.WriteLine("Thursday selected");
-                    break;
-                case Days.Friday:
-                    Console.WriteLine("Friday selected");
-                    break;
-                case Days.Saturday:
-                    Console.WriteLine("Saturday selected");
-                    break;
-                case Days.Sunday:
-                    Console.WriteLine("Sunday selected");
-                    break;
+                Console.WriteLine(item);
             }
+            
         }
     }
 }
